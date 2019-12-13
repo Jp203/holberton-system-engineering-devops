1264501c1bb301bdcf2c47d94d3c66b1b580b570
Jp203 <jp203@icloud.com>
2019-12-12 23:43:37 -0500

    Disable password login

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+
+# Disable password login, private key only
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
